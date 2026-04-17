e61868fcf4798329a719a3310e59e6640705464d
Joshua Deaton <josh.deaton13@gmail.com>
2026-04-17 19:58:03 -0400

    Track tina-lock.json, gitignore tina/__generated__

--- tina/__generated__/client.ts
+++
@@ -1,5 +0,0 @@
-import { createClient } from "tinacms/dist/client";
-import { queries } from "./types";
-export const client = createClient({ url: 'http://localhost:4001/graphql', token: 'b020d4b74cfafbca08c67e9375e861eb2d40d5f3', queries,  });
-export default client;
-  
